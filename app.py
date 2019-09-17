6a71b4a92c2d1ed77362c5faedba1758e3b49b77
John <jgbstephenson@gmail.com>
2019-09-17 17:27:47 +1000

    replaced model and labels

--- app.py
+++ app.py
@@ -27,10 +27,17 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 app = Flask(__name__)
 
+menu = {1: {'id': 1, 'name': 'person'}, 2: {'id': 2, 'name': 'bicycle'}, 3: {'id': 3, 'name': 'car'}, 4: {'id': 4, 'name': 'motorcycle'}, 5: {'id': 5, 'name': 'airplane'}, 6: {'id':
+6, 'name': 'bus'}, 7: {'id': 7, 'name': 'train'}, 8: {'id': 8, 'name': 'truck'}, 9: {'id': 9, 'name': 'boat'}, 10: {'id': 10, 'name': 'traffic light'}, 11: {'id': 11, 'name':
+'fire hydrant'}, 13: {'id': 13, 'name': 'stop sign'}, 14: {'id': 14, 'name': 'parking meter'}, 15: {'id': 15, 'name': 'bench'}, 16: {'id': 16, 'name': 'bird'}, 17: {'id': 17,
+'name': 'cat'}, 18: {'id': 18, 'name': 'dog'}, 19: {'id': 19, 'name': 'horse'}, 20: {'id': 20, 'name': 'sheep'}, 21: {'id': 21, 'name': 'cow'}, 22: {'id': 22, 'name': 'elephant'}, 23: {'id': 23, 'name': 'bear'}, 24: {'id': 24, 'name': 'zebra'}, 25: {'id': 25, 'name': 'giraffe'}, 27: {'id': 27, 'name': 'backpack'}, 28: {'id': 28, 'name': 'umbrella'}, 31: {'id': 31, 'name': 'handbag'}, 32: {'id': 32, 'name': 'tie'}, 33: {'id': 33, 'name': 'suitcase'}, 34: {'id': 34, 'name': 'frisbee'}, 35: {'id': 35, 'name': 'skis'}, 36:
+{'id': 36, 'name': 'snowboard'}, 37: {'id': 37, 'name': 'sports ball'}, 38: {'id': 38, 'name': 'kite'}, 39: {'id': 39, 'name': 'baseball bat'}, 40: {'id': 40, 'name': 'baseball glove'}, 41: {'id': 41, 'name': 'skateboard'}, 42: {'id': 42, 'name': 'surfboard'}, 43: {'id': 43, 'name': 'tennis racket'}, 44: {'id': 44, 'name': 'bottle'}, 46: {'id': 46, 'name': 'wine glass'}, 47: {'id': 47, 'name': 'cup'}, 48: {'id': 48, 'name': 'fork'}, 49: {'id': 49, 'name': 'knife'}, 50: {'id': 50, 'name': 'spoon'}, 51: {'id': 51, 'name': 'bowl'}, 52: {'id': 52, 'name': 'banana'}, 53: {'id': 53, 'name': 'apple'}, 54: {'id': 54, 'name': 'sandwich'}, 55: {'id': 55, 'name': 'orange'}, 56: {'id': 56, 'name': 'broccoli'}, 57: {'id': 57, 'name': 'carrot'}, 58: {'id': 58, 'name': 'hot dog'}, 59: {'id': 59, 'name': 'pizza'}, 60: {'id': 60, 'name': 'donut'}, 61: {'id': 61, 'name': 'cake'},
+62: {'id': 62, 'name': 'chair'}, 63: {'id': 63, 'name': 'couch'}, 64: {'id': 64, 'name': 'potted plant'}, 65: {'id': 65, 'name': 'bed'}, 67: {'id': 67, 'name': 'dining table'}, 70: {'id': 70, 'name': 'toilet'}, 72: {'id': 72, 'name': 'tv'}, 73: {'id': 73, 'name': 'laptop'}, 74: {'id': 74, 'name': 'mouse'}, 75: {'id': 75, 'name': 'remote'}, 76: {'id': 76, 'name': 'keyboard'}, 77: {'id': 77, 'name': 'cell phone'}, 78: {'id': 78, 'name': 'microwave'}, 79: {'id': 79, 'name': 'oven'}, 80: {'id': 80, 'name': 'toaster'}, 81: {'id': 81, 'name': 'sink'}, 82: {'id': 82, 'name': 'refrigerator'}, 84: {'id': 84, 'name': 'book'}, 85: {'id': 85, 'name': 'clock'}, 86: {'id': 86, 'name': 'vase'}, 87: {'id':
+87, 'name': 'scissors'}, 88: {'id': 88, 'name': 'teddy bear'}, 89: {'id': 89, 'name': 'hair drier'}, 90: {'id': 90, 'name': 'toothbrush'}}
 
-def getI420FromBase64(codec):
+def getI420FromBase64(b64in):
     """ Convert image from a base64 bytes stream to an image. """
-    base64_data = codec['image']
+    base64_data = b64in
     print(base64_data)
     byte_data = base64.b64decode(base64_data)
     image_data = BytesIO(byte_data)
@@ -89,60 +96,66 @@ def getAllItems():
     return itemstr
 
 
+@app.route('/')
+def homepage():
+    return 'This backend serves as a REST API for the React front end. Try running npm start from the self-checkout folder.'
+
 @app.route('/detection', methods=['POST'])
 def detection():
-    request.get_data()
+    data = request.get_json()
+    
+    datastring = data['image']
 
     # Load in an image to object detect and preprocess it
-    img_data = getI420FromBase64(request.get_json())
-    x_input = np.expand_dims(img_data, axis=0)  # add an extra dimention.
+    img_data = getI420FromBase64(request.data)
 
-    # Setting initial detection time, so execution time can be calculated.
+    img_data = Image.open(image)
+
+    x_input = np.expand_dims(img_data, axis=0) # add an extra dimention.
+
+    # Setting initial detection time, so execution time can be calculated.    
     t_det = time.time()
 
     # Get the predictions (output of the softmax) for this image
-    tf_results_det = sess_det.run(
-        [output_tensor_det, detection_boxes, detection_scores, detection_num], {input_tensor_det: x_input})
+    tf_results_det = sess_det.run([output_tensor_det,detection_boxes,detection_scores,detection_num], {input_tensor_det : x_input})
 
     dt_det = time.time() - t_det
     app.logger.info("Execution time: %0.2f" % (dt_det * 1000.))
 
     # Different results arrays
     predictions_det = tf_results_det[0]
-    prediction_scores_det = tf_results_det[2]
-    prediction_boxes_det = tf_results_det[1]
-    prediction_num_det = tf_results_det[3]
+    prediction_scores_det=tf_results_det[2]
+    prediction_boxes_det=tf_results_det[1]
+    prediction_num_det=tf_results_det[3]
 
-    # print("----------------------------")
-    # print(predictions_det)
-    # print(prediction_scores_det)
-    # print("----------------------------")
 
     threshold = 0.85
 
-    num = int(prediction_num_det)
-    predict_list = predictions_det[0].astype(int).tolist()
+    num=int(prediction_num_det)
+    predict_list=predictions_det[0].astype(int).tolist()
     scores = prediction_scores_det[0]
-    label = []
+    label=[]
 
     for i in range(num):
-        new_item = {}
+        
         if scores[i] > threshold:
-            prediction_label = str(predict_list[i])
-            obj_name = prediction_label
-            obj_price = prediction_label
+            prediction_label = predict_list[i]
+            obj_name = menu[prediction_label]['name']
+            
 
-            new_item = {'id': randint(0, 100000),
-                        'name': obj_name,
-                        'quantity': 1,
-                        'price': obj_price}
+            
 
-            label.append(new_item)
-
+            label.append(obj_name)
+           
     print("number and list of items that above the threshold")
     print(len(label))
     print(label)
-    return jsonify(label)
+    return label
+
+
+##################################################
+# Starting the server
+##################################################
 
 
 if __name__ == '__main__':
@@ -152,39 +165,38 @@ if __name__ == '__main__':
     # Create the session that we'll use to execute the model
     sess_config = tf.ConfigProto(
         log_device_placement=False,
-        allow_soft_placement=True
+        allow_soft_placement = True
     )
 
     print('Loading Model...')
-    # G Read the graph definition file
+    #G Read the graph definition file
     with open(MODEL_DETECT_PATH, 'rb') as k:
-        graph_def = tf.GraphDef()
+        graph_def=tf.GraphDef()
         graph_def.ParseFromString(k.read())
 
-    # G Load the graph stored in `graph_def` into `graph`
+    #G Load the graph stored in `graph_def` into `graph`
     graph = tf.Graph()
     with graph.as_default():
         tf.import_graph_def(graph_def, name='')
 
-    # G Enforce that no new nodes are added
+    #G Enforce that no new nodes are added
     graph.finalize()
 
     sess_det = tf.Session(graph=graph, config=sess_config)
 
     print('Done.')
 
-    input_op_det = graph.get_operation_by_name(
-        'image_tensor')  # mport/Preprocessor/map/Shape
+    input_op_det = graph.get_operation_by_name('image_tensor') # mport/Preprocessor/map/Shape
     input_tensor_det = input_op_det.outputs[0]
-    output_op_det = graph.get_operation_by_name(
-        'detection_classes')  # or num_detections
+    output_op_det = graph.get_operation_by_name('detection_classes')#or num_detections
     output_tensor_det = output_op_det.outputs[0]
 
-    detection_boxes_op = graph.get_operation_by_name('detection_boxes')
-    detection_boxes = detection_boxes_op.outputs[0]
-    detection_scores_op = graph.get_operation_by_name('detection_scores')
-    detection_scores = detection_scores_op.outputs[0]
-    detection_num_op = graph.get_operation_by_name('num_detections')
-    detection_num = detection_num_op.outputs[0]
+    detection_boxes_op=graph.get_operation_by_name('detection_boxes')
+    detection_boxes=detection_boxes_op.outputs[0]
+    detection_scores_op=graph.get_operation_by_name('detection_scores')
+    detection_scores=detection_scores_op.outputs[0]
+    detection_num_op=graph.get_operation_by_name('num_detections')
+    detection_num=detection_num_op.outputs[0]
 
-    app.run(debug=True, host='0.0.0.0')
+    
+    app.run(debug=False, host='0.0.0.0')
