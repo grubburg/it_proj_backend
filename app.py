70585d4fe73e8af69ddc26080514059ff269b14f
John Stephenson <jgbstephenson@gmail.com>
2019-09-17 14:26:03 +1000

    changed API key

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask import Flask, request
 import os
 
 import firebase_admin
+from firebase_admin import auth
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account
 # GCScert = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 
-cred = credentials.Certificate('./it-proj-backend-250602-58ddd0292162.json')
+cred = credentials.Certificate(
+    './it-proj-backend-firebase-adminsdk-pnizj-2ab146748a.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -18,24 +20,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "compu-global-hyper-mega-net was here!"
+    return "compu-global-hyper-mega-net was here!"
 
 
-@app.route('/items/add', methods = ['POST'])
+@app.route('/items/add', methods=['POST'])
 def addItem():
-	data = request.get_json()
-	db.collection(u'items').add(data)
-	return str(data)
+    data = request.get_json()
+    db.collection(u'items').add(data)
+    return str(data)
+
 
 @app.route('/items/')
 def getAllItems():
-	items = db.collection(u'items').stream()
-	itemstr = ''
-	for item in items:
-		
-		itemstr += str(item.to_dict()) + '\n'
-	return itemstr
+    items = db.collection(u'items').stream()
+    itemstr = ''
+    for item in items:
 
+        itemstr += str(item.to_dict()) + '\n'
+    return itemstr
 
 
 if __name__ == '__main__':
