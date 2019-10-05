cb446e0a58182e09820e008506e666a0a1c1f970
John <jgst@student.unimelb.edu.au>
2019-10-05 11:21:49 +0000

    renamed cred files

--- objectify/__init__.py
+++ objectify/__init__.py
@@ -11,7 +11,7 @@ from firebase_admin import storage
 from google.cloud import storage as gstorage
 
 cred = credentials.Certificate(
-    './it-proj-backend-firebase-adminsdk-pnizj-fc9aa8b559.json')
+    './it-proj-backend-firebase-adminsdk-pnizj-2ab146748a.json')
 firebase_admin.initialize_app(cred, {
     'storageBucket' : 'it-proj-backend.appspot.com'
 })
@@ -24,7 +24,7 @@ def create_app():
         
         g.db = firestore.client()
         g.bucket = storage.bucket()
-        g.client = gstorage.Client.from_service_account_json('./it-proj-backend-firebase-adminsdk-pnizj-fc9aa8b559.json')
+        g.client = gstorage.Client.from_service_account_json('./it-proj-backend-firebase-adminsdk-pnizj-2ab146748a.json')
         g.gstorage = gstorage
         from .family import family_routes
         from .item import item_routes
