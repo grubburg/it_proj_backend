cf99b62e927bbb00b7eaba588bc2a38edda70d31
John Stephenson <jgbstephenson@gmail.com>
2019-10-18 00:26:53 +1100

    reset tokens and turned off debugging for production

--- objectify/__init__.py
+++ objectify/__init__.py
@@ -11,9 +11,9 @@ from firebase_admin import storage
 from google.cloud import storage as gstorage
 
 cred = credentials.Certificate(
-    'it-proj-backend-firebase-adminsdk-pnizj-fc9aa8b559.json')
+    'it-proj-backend-firebase-adminsdk-pnizj-2ab146748a.json')
 firebase_admin.initialize_app(cred, {
-    'storageBucket' : 'it-proj-backend.appspot.com'
+    'storageBucket': 'it-proj-backend.appspot.com'
 })
 
 
@@ -21,10 +21,11 @@ def create_app():
     app = Flask(__name__)
 
     with app.app_context():
-        
+
         g.db = firestore.client()
         g.bucket = storage.bucket()
-        g.client = gstorage.Client.from_service_account_json('it-proj-backend-firebase-adminsdk-pnizj-fc9aa8b559.json')
+        g.client = gstorage.Client.from_service_account_json(
+            'it-proj-backend-firebase-adminsdk-pnizj-2ab146748a.json')
         g.gstorage = gstorage
         from .family import family_routes
         from .item import item_routes
